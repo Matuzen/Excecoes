2fe1d327eb43d0d412986c4dc928e60055dc6265
Matheus Alexandre <matheusalexmc@gmail.com>
2022-11-01 17:53:05 -0300

    Exemplo 1 de 3 - Reserva de Hotel

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excecoes.Entities;
+
+namespace Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+        }
+    }
+}
